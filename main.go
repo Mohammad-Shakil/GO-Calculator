6eea15d9b138e78b310084beda7f855a9e239b92
Shakil <md.fahim.sakil@gmail.com>
2026-09-25 11:45:29 +0600

    initial codes

--- main.go
+++ main.go
@@ -6,4 +6,16 @@ func main() {
 
 	fmt.Println("--- Calculator ---")
 
+	for {
+
+		var input1 string
+		fmt.Printf("Enter value / Q to exit")
+		fmt.Scanln(&input1)
+		fmt.Printf("\n----Choose operator----")
+		fmt.Println("1: +")
+		fmt.Println("2: -")
+		fmt.Println("3: *")
+		fmt.Println("4: /")
+
+	}
 }
